0ce7fb3aa3daf2d346707096b4e605cf5d3d1e34
liu <1101158043@qq.com>
2019-06-14 16:35:04 +0800

    UserService

---
+++ src/main/java/com/qfedu/service/UserService.java
@@ -0,0 +1,7 @@
+package com.qfedu.service;
+
+/**
+ * Created by Administrator on 2019/6/14 0014.
+ */
+public interface UserService {
+}
---
+++ src/main/java/com/qfedu/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.qfedu.service.impl;
+
+import com.qfedu.service.UserService;
+
+/**
+ * Created by Administrator on 2019/6/14 0014.
+ */
+public class UserServiceImpl implements UserService {
+}
