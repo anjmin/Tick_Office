4d53989bce3f24057f2a2a69d84f7364209ce227
liu <1101158043@qq.com>
2019-06-14 16:57:05 +0800

    UserServiceiml

--- src/main/java/com/qfedu/service/impl/UserServiceImpl.java
+++ src/main/java/com/qfedu/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.qfedu.service.impl;
 
+import com.qfedu.dao.UserDao;
 import com.qfedu.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired(required = false)
+    private UserDao userDao;
 }
