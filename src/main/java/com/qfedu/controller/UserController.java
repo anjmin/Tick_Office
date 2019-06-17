42f6f24aa42697fe8b1ed50f07c0cb87dc096fd3
liu <1101158043@qq.com>
2019-06-17 17:22:56 +0800

    user

--- src/main/java/com/qfedu/controller/UserController.java
+++ src/main/java/com/qfedu/controller/UserController.java
@@ -9,21 +9,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
-
     @RequestMapping("/login.do")
     public String login(String no,String password){
-
+        System.out.println("输入的用户名："+no+"输入的密码："+password);
         UsernamePasswordToken token = new UsernamePasswordToken(no, password);
         Subject subject = SecurityUtils.getSubject();
         try {
             subject.login(token);
-            return "redirect:/main.html";
+            return "redirect:/index.html";
         } catch (AuthenticationException e) {
             e.printStackTrace();
             return "redirect:/login.html";
         }
+    }
+
+    //@RequiresPermissions("user:list")
+    @RequestMapping("/test.do")
+    public String test(){
+        return "redirect:/main.html";
+    }
+    //@RequiresPermissions("user:sss")
+    @RequestMapping("/test2.do")
+    public String test2(){
+        return "redirect:/main.html";
     }
 
 }
