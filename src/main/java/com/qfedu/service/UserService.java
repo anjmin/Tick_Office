e47225b90b8ed3f1b38eba147fc1e56db9c9cc8c
liu <1101158043@qq.com>
2019-06-21 20:16:18 +0800

    roleAll

--- src/main/java/com/qfedu/service/UserService.java
+++ src/main/java/com/qfedu/service/UserService.java
@@ -1,5 +1,8 @@
 package com.qfedu.service;
 
+import com.qfedu.pojo.Role;
+
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,6 +13,14 @@ public interface UserService {
     //用户角色展示列表、根据关键字查询
     Map<String,Object> findAllByNoAndFg(String no, Integer flag, Integer page, Integer limit);
 
+    //删除用户角色
     void deleteById(Integer id);
+
+    //查找用户角色
+    List<Role> findAllRole();
+
+    //修改用户角色
+    void userRoleEdit(Integer id, Integer[] rids);
+
 
 }
--- src/main/java/com/qfedu/service/impl/UserServiceImpl.java
+++ src/main/java/com/qfedu/service/impl/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package com.qfedu.service.impl;
 
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.qfedu.dao.UserDao;
+import com.qfedu.pojo.Role;
 import com.qfedu.pojo.User;
 import com.qfedu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +26,11 @@ public class UserServiceImpl implements UserService {
         PageHelper.startPage(page,limit);
         List<User> list = userDao.findAllByNoAndFg(no, flag);
         Map<String, Object> map = new HashMap<>();
+       long to = userDao.findByAllT();
 
         //总记录数
-        long total = ((Page)list).getTotal();
-        //总页数
-        int pages = ((Page)list).getPages();
+        long total = to ;
+
         map.put("code",0);
         map.put("msg","");// 总记录数
         map.put("count", total);
@@ -39,10 +39,23 @@ public class UserServiceImpl implements UserService {
         return map;
     }
 
+    //删除用户角色
     @Override
     public void deleteById(Integer id) {
         userDao.deleteById(id);
     }
 
+    //查找用户角色
+    @Override
+    public List<Role> findAllRole() {
+        return userDao.findAllRole();
+    }
+
+    //查找修改用户角色
+    @Override
+    public void userRoleEdit(Integer id, Integer[] rids) {
+        userDao.deleteUserRoleById(id);
+        userDao.insertUserRole(id,rids);
+    }
 
 }
